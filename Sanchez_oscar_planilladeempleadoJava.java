d2085bdce6a28b496defeb97a2dc39a47bf95dad
oscarsm18 <oscargabriel8018@gmail.com>
2025-04-25 13:52:06 -0600

    Add files via upload

---
+++ Sanchez_oscar_planilladeempleadoJava.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio.pkg1;
+
+/**
+ *
+ * @author oscarsanchez
+ */
+public class Sanchez_oscar_planilladeempleadoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Boleta del empleado
+        String nombre_empleado="Javier";
+        int horat_mensual=40;
+        double tarifa_hora=10.85;
+        double salario_semanal=80.50;
+        
+        System.out.println("---- Boleta del empleado ----");
+        System.out.println(" Nombre del empleado: "+nombre_empleado+"\n");
+        System.out.println(" Horas de trabajado mensual "+horat_mensual+"\n");
+        System.out.println(" Tarifa por hora "+tarifa_hora+"\n");
+        System.out.println(" Salario del empleado semanal "+salario_semanal+"\n");
+    }
+    
+}
